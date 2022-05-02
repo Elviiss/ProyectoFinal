9dbf1694fc098441901c8ae496c33c48e143eaf3
PabloELrivas <99806891+PabloMuYAnYO@users.noreply.github.com>
2022-05-02 09:33:56 +0200

    Add files via upload

---
+++ personaje.py
@@ -0,0 +1,18 @@
+class Personaje:
+      def __init__(self):
+        self.nombre = ""
+        self.salud = 1
+        self.salud_max = 1
+    
+class Jugador(Personaje):
+      def __init__(self):
+        Personaje.__init__(self)
+        self.estado = 'normal'
+        self.salud = 10
+        self.salud_max = 10
+
+
+p = Jugador()
+p.nombre = input("Cual es el nombre del personaje? ")
+print ("(Escribe ayuda para obtener una lista de acciones)n")
+print ("%s bienvenido a Tame, preparate para la aventura.") % p.nombre
