6a444983d6af1af2c17dce0ebba40547b8a96bb8
Alejandro Navarro <drojannp@gmail.com>
2022-05-02 09:19:54 +0200

    Creamos el archivo principal

---
+++ a.py
@@ -0,0 +1,5 @@
+import time 
+año = "Año 781 Planeta Tame… Años después de una guerra nuclear, comenzaron a haber casos extraños de canibalismo, lo peor ocurrió cuando se descubrió que fue por un experimento fallido del científico Gregel. Comenzó el apocalipsis, se descubrió que no era canibalismo, era gente que había muerto. Los muertos se estaban comiendo a las personas vivas, cuando esto ocurría ellos también se convertían en estos seres."
+for i in range(len(año)):
+    print(año[i], end="")
+    time.sleep(0.04)
