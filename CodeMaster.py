426af017f7b99de6dc28430048c4e274021b2d9b
= <=>
2022-05-11 13:09:47 +0200

    Creando juego de adivinar un codigo

---
+++ CodeMaster.py
@@ -0,0 +1,51 @@
+import random
+
+digitos = ('0','1','2','3','4','5','6','7','8','9')
+
+codigo = ''
+for i in range(4):
+    candidato = random.choice(digitos)
+    
+    while candidato in codigo:
+        candidato = random.choice(digitos)
+    codigo = codigo + candidato
+
+print('''
+    
+██████╗ ██████╗ ██████╗ ███████╗    ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ 
+██╔════╝██╔═══██╗██╔══██╗██╔════╝    ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+██║     ██║   ██║██║  ██║█████╗      ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝
+██║     ██║   ██║██║  ██║██╔══╝      ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗
+╚██████╗╚██████╔╝██████╔╝███████╗    ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║
+ ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝  
+      
+      ''')
+print ("Bienvenido/a!")
+print ("Tienes que adivinar un numero de", 4, "cifras distintas.")
+propuesta = input("¿Que codigo propones?: ")
+
+
+intentos = 1
+while propuesta != codigo:
+    intentos = intentos + 1
+    aciertos = 0
+    coincidencias = 0
+
+    for i in range(4):
+        if propuesta[i] == codigo[i]:
+            aciertos = aciertos + 1
+        elif propuesta[i] in codigo:
+            coincidencias = coincidencias + 1
+    print ("Tu propuesta (", propuesta, ") tiene", aciertos, \
+          "aciertos y ", coincidencias, "coincidencias.")
+    print(codigo)
+    propuesta = input("Propón otro codigo: ")
+
+print ("Felicitaciones! Adivinaste el codigo en", intentos, "intentos.")
+
+if intentos < 3:
+    print("eh... bro, COMO?????")
+if intentos == 3:
+    print("A la tercera va la vencida!!!")
+if intentos <= 5:
+    print("Increible!!")
