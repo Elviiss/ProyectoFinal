a2096c2f3f423ee330f4637116a9d0a7328af5aa
PabloELrivas <99806891+PabloMuYAnYO@users.noreply.github.com>
2022-05-17 10:23:46 +0200

    Implementando corrección de error 4 dígitos

--- CodeMaster.py
+++ CodeMaster.py
@@ -50,7 +50,7 @@ while len(propuesta) <= 4:
                 print("A la tercera va la vencida!!!")
             if intentos == 5:
                 print("Increible!! PD: Tranqui no hay rima jajaja")
-            if intentos in range (5,19):
+            if intentos in range (6,19):
                 print("Nada mal!")
             if intentos in range (20,50):
                 print("Bien, has tardado un poco, pero lo conseguiste!")
@@ -60,37 +60,4 @@ while len(propuesta) <= 4:
                 print("Como has llegado tan lejos?!")
 else:
     print("Error, tu codigo debe tener 4 digitos.")
-<<<<<<< HEAD
-    propuesta = input("Propón otro codigo: ")
-    while propuesta != codigo:
-        intentos = intentos + 1
-        aciertos = 0
-        coincidencias = 0
-
-    for i in range(4):
-        if propuesta[i] == codigo[i]:
-            aciertos = aciertos + 1
-        elif propuesta[i] in codigo:
-            coincidencias = coincidencias + 1
-            print ("Tu propuesta (", propuesta, ") tiene", aciertos, \
-                  "aciertos y ", coincidencias, "coincidencias.")
-            print(codigo)
-            if propuesta == codigo:
-                print ("Felicitaciones! Adivinaste el codigo en", intentos, "intentos.")
-            if intentos < 3:
-                print("eh... bro, COMO?????")
-            if intentos == 3:
-                print("A la tercera va la vencida!!!")
-            if intentos == 5:
-                print("Increible!! PD: Tranqui no hay rima jajaja")
-            if intentos in range (5,19):
-                print("Nada mal!")
-            if intentos in range (20,50):
-                print("Bien, has tardado un poco, pero lo conseguiste!")
-            if intentos in range (50,100):
-                print("Vaya, tienes paciencia lo reconozco!")
-            if intentos in range > 100:
-                print("Como has llegado tan lajos?!")
-=======
     print("GAME OVER :(")
->>>>>>> 13f7316a8e0b0cfdd4557b67254102943617287e
