fa5fe339df9df0c6ff7f63cd608cecd75f56f2a1
Alejandro Navarro <drojannp@gmail.com>
2022-05-19 10:58:12 +0200

    Arreglo un error de las palas y creo la pelota

--- pong.py
+++ pong.py
@@ -13,6 +13,7 @@ jugador1 = turtle.Turtle()
 jugador1.speed(0)
 jugador1.color("white")
 jugador1.shape("square")
+jugador1.penup()
 jugador1.goto(-350, 0) 
 jugador1.shapesize(stretch_wid=5, stretch_len = 1)
 
@@ -22,11 +23,16 @@ jugador2 = turtle.Turtle()
 jugador2.speed(0)
 jugador2.color("white")
 jugador2.shape("square")
+jugador2.penup()
 jugador2.goto(350, 0) 
 jugador2.shapesize(stretch_wid=5, stretch_len = 1)
 
-
-
+##PELOTA
+pelota = turtle.Turtle()
+pelota.speed(0)
+pelota.color("white")
+pelota.shape("circle")
+pelota.penup()
 
 ##BUCLE PRINCIPAL
 while True:
